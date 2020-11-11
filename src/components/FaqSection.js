cd3d4d8fbb25333a173f5d8ce29613fc5120a49b
Ikraam <IbnDaanis@Gmail.com>
2020-11-11 21:49:46 +0000

    Added Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+export const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>Any Questions? <span>FAQ</span></h2>
+      {/* Question 1 */}
+      <div className="question">
+        <h4>How Do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nam!</p>
+        </div>
+      </div>
+      {/* Question 2 */}
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nam!</p>
+        </div>
+      </div>
+      {/* Question 3 */}
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nam!</p>
+        </div>
+      </div>
+      {/* Question 4 */}
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, nam!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
