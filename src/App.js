6c1d548e86e913f332aa5d69aa587ab265f60ae7
Ikraam <IbnDaanis@Gmail.com>
2020-11-12 16:24:05 +0000

    Styled the Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { GlobalStyle } from "./components/GlobalStyle"
-import { AboutUs } from "./pages/AboutUs"
+import { GlobalStyle } from './components/GlobalStyle'
+import { Nav } from './components/Nav'
+import { AboutUs } from './pages/AboutUs'
 
 export const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   )
