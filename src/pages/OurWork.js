5084c5fee45c17d3a732aa812d46319cde9a4cce
Ikraam <IbnDaanis@Gmail.com>
2020-11-13 20:47:16 +0000

    Added scroll animations on OurWork page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -11,8 +11,9 @@ import {
   lineAnim,
   slider,
   sliderContainer,
+  scrollReveal,
 } from '../animation'
-
+import { useScroll } from '../components/useScroll'
 const Work = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
@@ -21,7 +22,7 @@ const Work = styled(motion.div)`
     padding: 1rem 0;
   }
 `
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.3rem;
@@ -61,6 +62,8 @@ const Frame4 = styled(Frame1)`
 `
 
 export const OurWork = () => {
+  const [element, controls] = useScroll()
+  const [element2, controls2] = useScroll()
   return (
     <Work
       variants={pageAnimation}
@@ -84,16 +87,26 @@ export const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie
+        ref={element}
+        variants={scrollReveal}
+        animate={controls}
+        initial='hidden'
+      >
         <h2>The Racer</h2>
-        <div className='line'></div>
+        <motion.div variants={lineAnim} className='line'></motion.div>
         <Link to='/work/the-racer'>
           <img src={theracer} alt='The Racer' />
         </Link>
       </Movie>
-      <Movie>
+      <Movie
+        ref={element2}
+        variants={scrollReveal}
+        animate={controls2}
+        initial='hidden'
+      >
         <h2>Good Times</h2>
-        <div className='line'></div>
+        <motion.div variants={lineAnim} className='line'></motion.div>
         <Link to='/work/good-times'>
           <img src={goodtimes} alt='Good Times' />
         </Link>
